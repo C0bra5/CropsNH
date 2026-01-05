c98ce816ee024f65a6dfba9d618c74cc28f564bb
Guillaume Mercier <10gui-gui10@live.ca>
2026-01-05 04:55:14 -0500

    doc

--- src/main/java/com/gtnewhorizon/cropsnh/compatibility/TiC/CropsNHTiCHarvestHandler.java
+++ src/main/java/com/gtnewhorizon/cropsnh/compatibility/TiC/CropsNHTiCHarvestHandler.java
@@ -19,6 +19,7 @@ public class CropsNHTiCHarvestHandler implements CropHarvestHandler {
     @Override
     public boolean tryHarvest(ItemStack stack, EntityPlayer player, World world, int x, int y, int z) {
         if (!(world.getTileEntity(x, y, z) instanceof ICropStickTile crop)) return false;
+        // allow weed removal just like a spade would.
         if (crop.hasWeed()) {
             if (crop.isMature()) {
                 crop.dropItem(CropsNHUtils.getWeedDrop(1));
