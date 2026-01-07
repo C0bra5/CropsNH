0603e6656fb9e0921459492bc097564c01a8f9bb
Guillaume Mercier <10gui-gui10@live.ca>
2026-01-07 02:34:13 -0500

    fix coffe drop?

--- src/main/java/com/gtnewhorizon/cropsnh/crops/food/CropCoffee.java
+++ src/main/java/com/gtnewhorizon/cropsnh/crops/food/CropCoffee.java
@@ -9,12 +9,15 @@ import com.gtnewhorizon.cropsnh.api.SeedShape;
 import com.gtnewhorizon.cropsnh.crops.abstracts.CropFood;
 
 import gregtech.api.enums.Materials;
+import gregtech.api.enums.OrePrefixes;
+import gregtech.api.util.GTOreDictUnificator;
 
 public class CropCoffee extends CropFood {
 
     public CropCoffee() {
         super("coffee", new Color(0x3D1E00), new Color(0x964F05));
-        this.addDrop(Materials.Coffee.getDust(1), 10_000);
+
+        this.addDrop(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Coffee, 1), 10_000);
         // likes warm temperatures, usually found in the same area as rainforests.
         this.addLikedBiomes(BiomeDictionary.Type.HOT, BiomeDictionary.Type.LUSH, BiomeDictionary.Type.WET);
     }
